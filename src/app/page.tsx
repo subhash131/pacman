0bacc8d8c1a9178c1689ca65d8c742f3dd9b0ba6
subhash131 <subhashnayak131@gmail.com>
2024-07-16 02:41:55 +0530

    add web3 components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 "use client";
-import { Profile } from "@/components/test/demo";
+import Canvas from "@/components/canvas";
+import { Profile } from "@/components/web3/demo";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="w-screen h-screen">
-      {/* <Canvas /> */}
+      <Canvas />
       <Suspense fallback={"loading..."}>
         <Profile />
       </Suspense>
-      {/* <div className="absolute top-0 left-0 size-full">
+      <div className="absolute top-0 left-0 size-full">
         <button className="p-2 bg-white text-black rounded-lg">connect</button>
-      </div> */}
+      </div>
     </main>
   );
 }
