be9c1242710904818915e9b5ba2cfbacc0b8f8b1
subhash131 <subhashnayak131@gmail.com>
2024-07-18 21:57:22 +0530

    update hero styles

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
     <div
       className={`relative w-full h-full text-white px-10 flex gap-2 items-center flex-col text-center justify-center pb-20 ${poppins.className}`}
     >
-      <div className="absolute top-14 gap-2 left-64 rotate-12 flex items-center justify-center">
+      <div className="absolute top-[8%] gap-2 left-[15%] rotate-12 flex items-center justify-center">
         <p className="text-xl font-bold drop-shadow-2xl lowercase bg-[#F1EDFE] text-[#5F01FF] rounded-lg p-4 rotate-6">
           1. place bet
         </p>
@@ -25,7 +25,7 @@ const Hero = () => {
           className="size-32"
         />
       </div>
-      <div className="absolute top-20 gap-2 right-44 flex flex-col items-center justify-center">
+      <div className="absolute top-[10%] gap-2 right-[12%] flex flex-col items-center justify-center">
         <p className="text-xl font-bold drop-shadow-2xl lowercase bg-[#F1EDFE] text-[#5F01FF] rounded-lg p-4 -rotate-6 ">
           2. Play game
         </p>
@@ -37,7 +37,7 @@ const Hero = () => {
           className="size-32 rotate-90"
         />
       </div>
-      <div className="absolute bottom-36 gap-2 left-60 flex flex-col items-center justify-center">
+      <div className="absolute bottom-[22%] gap-2 left-[18%] flex flex-col items-center justify-center">
         <Image
           src="/arrow.svg"
           alt="arrow"
